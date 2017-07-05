f7230ff1d7186354afb087ce50f4f4b8c1bd62ca
JurgenSchouppe <jurgen.soltronics@gmail.com>
2017-07-05 16:40:34 +0200

    Create sims.php
    sims.php

---
+++ sims.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();   
+    include 'functions.php';
+
+    echo "<head>";
+    echo "<title>FE SSL LABS TOOL</title>";
+    echo "<link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>";
+    echo "<link rel='stylesheet' type='text/css' href='css/font-awesome.min.css'>";
+    echo "<link rel='stylesheet' type='text/css' href='css/menu.css'>";
+    echo "<link rel='stylesheet' type='text/css' href='css/main.css'>";
+    echo "<link rel='stylesheet' type='text/css' href='css/rating.css'>";
+    echo "</head>";
+
+
+    echo "<body>";
+    echo "<div class='title'><center><H2>FE SSL LABS TOOL</H2></center></div>";
+    echo "<div class='icon-bar'>";
+            echo "<a href='index.php'><i class='fa fa-home'></i></a>";
+            echo "<a href='summary.php'><i class='fa fa-address-card'></i></a>";
+            echo "<a href='certs.php'><i class='fa fa-chain'></i></a>";
+            echo "<a href='details.php'><i class='fa fa-cog'></i></a>";
+            echo "<a href='prots.php'><i class='fa fa-user-secret'></i></a>";
+            echo "<a class='active' href='sims.php'><i class='fa fa-handshake-o'></i></a>";
+            echo "<a href='raw.php'><i class='fa fa-book'></i></a>";
+    echo "</div>";
+
+$site = $_SESSION['site'];
+$dirP = ('/data/sslscan/scans');
+$file2check = file_get_contents("$dirP/$site/latest.json");
+$attr2check = json_decode($file2check, true);
+$attr2check = $attr2check[0];
+
+    Hgrade();
+
+	
+
+	echo "<div class='wrap2'>";
+	    	echo "<div class='sims'>";
+		$isi=0;
+		 $simsnr =(count($attr2check['endpoints'][0]['details']['sims']['results']));
+		        echo "<p><b>Number of tested Handshakes :</b>$simsnr</p>\n";
+	                 while ($isi <= $simsnr -1 ) {
+        		        echo "<p><b>Simulation $isi\n</b></p>";
+		                foreach ($attr2check['endpoints'][0]['details']['sims']['results'][$isi]['client'] as $field => $value) {
+		                     echo "<p>&emsp;<b>$field :</b>$value</p>\n";
+																												                                              }
+																																	 $isi++;
+	                  }
+
+		echo "</div>";
+
+	echo "</div>";
+
+    echo "</body>";
+
+?>
